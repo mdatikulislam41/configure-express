f9dc7d4390a409c8b2dd8338eebd89f2cdb408e7
mdatikulislam41 <mdatikulislam41@gmail.com>
2026-02-19 23:48:19 +0600

    update

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { userRouter } = require("./routes/user.Router");
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/", userRouter);
 app.get("/", (req, res) => {
   res.send("Api running");
+});
+app.use((req, res) => {
+  res.status(505).json({
+    message: "not found",
+  });
 });
 app.get((error, req, res, next) => {
   res.status(505).json({
